ccc8294b493fbe63c4a971fbfac08d75f315a3ae
OscarZarcoG <danielzargon@gmail.com>
2026-01-30 14:29:48 -0600

    refactor LSP configuration

--- lua/lsp-config.lua
+++ lua/lsp-config.lua
@@ -1,10 +1,94 @@
 local lspconfig = require('lspconfig')
+local navic = require("nvim-navic")
 
-lspconfig.lua_ls.setup({})
-lspconfig.ts_ls.setup({})
-lspconfig.tailwindcss.setup({})
-lspconfig.intelephense.setup({})
-lspconfig.html.setup({})
-lspconfig.cssls.setup({})
-lspconfig.jsonls.setup({})
-lspconfig.eslint.setup({})
+local on_attach = function(client, bufnr)
+    if client.server_capabilities.documentSymbolProvider then
+        navic.attach(client, bufnr)
+    end
+end
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+-- Lua
+lspconfig.lua_ls.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { 'vim' }
+            },
+            workspace = {
+                library = vim.api.nvim_get_runtime_file("", true),
+                checkThirdParty = false,
+            },
+            telemetry = {
+                enable = false,
+            },
+        }
+    }
+})
+
+-- TypeScript/JavaScript
+lspconfig.ts_ls.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+    root_dir = lspconfig.util.root_pattern("package.json", "tsconfig.json", "jsconfig.json"),
+    single_file_support = false,
+})
+
+-- TailwindCSS
+lspconfig.tailwindcss.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+    root_dir = lspconfig.util.root_pattern(
+        "tailwind.config.js",
+        "tailwind.config.cjs",
+        "tailwind.config.mjs",
+        "tailwind.config.ts"
+    ),
+    single_file_support = false,
+})
+
+-- PHP
+lspconfig.intelephense.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+    root_dir = lspconfig.util.root_pattern("composer.json", ".git"),
+    single_file_support = false,
+})
+
+-- HTML
+lspconfig.html.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+    filetypes = { "html" },
+})
+
+-- CSS
+lspconfig.cssls.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+})
+
+-- JSON
+lspconfig.jsonls.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+})
+
+-- ESLint
+lspconfig.eslint.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+    root_dir = lspconfig.util.root_pattern(
+        ".eslintrc",
+        ".eslintrc.js",
+        ".eslintrc.cjs",
+        ".eslintrc.yaml",
+        ".eslintrc.yml",
+        ".eslintrc.json",
+        "eslint.config.js"
+    ),
+    single_file_support = false,
+})
